69c596be459e5ec80b7748ef3602d2040ddc3827
Julio Labarca <jlabarca25@gmail.com>
2020-07-22 23:12:12 +0100

    Command Manager

--- Assets/Scripts/Core/CommandManager.cs
+++ Assets/Scripts/Core/CommandManager.cs
@@ -1,25 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using com.jlabarca.cpattern.Core.Commands;
+using UnityEngine;
 
 namespace com.jlabarca.cpattern.Core
 {
-    public class Executor : MonoBehaviour
+    public class CommandManager : MonoBehaviour
     {
+        internal static CommandManager Instance;
+        private int _index;
+
         public State state;
         public float executionPeriod;
         private float _elapsed;
 
+        void Awake(){
+
+            if (Instance == null){
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            } else {
+                Destroy(this);
+            }
+        }
+
         private void Start()
         {
-            state = new State();
+            state = new State
+            {
+                commands = new List<ICommand>()
+            };
         }
 
         private void FixedUpdate()
         {
             _elapsed += Time.deltaTime;
-            if (_elapsed > executionPeriod)
-            {
-                state.ExecuteNextCommand();
-            }
+
+            if (!(_elapsed > executionPeriod)) return;
+
+            state.ExecuteNextCommand();
+            _elapsed = 0;
+        }
+
+        public void AddCommand(ICommand command)
+        {
+            Debug.Log("AddCommand "+command.GetType().Name);
+            state.commands.Add(command);
         }
     }
 }
