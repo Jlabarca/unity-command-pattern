2aa11994b772fce598ab2ec3c4f421df9fb5e2a6
Julio Labarca <jlabarca25@gmail.com>
2020-07-26 14:24:11 +0100

    Navigation Path

---
+++ Assets/Scripts/Navigation/Path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace com.jlabarca.cpattern.Navigation
+{
+	public class Path {
+		public List<int> xPositions;
+		public List<int> yPositions;
+
+		public int count {
+			get {
+				return xPositions.Count;
+			}
+		}
+
+		public Path() {
+			xPositions = new List<int>(100);
+			yPositions = new List<int>(100);
+		}
+
+		public void Clear() {
+			xPositions.Clear();
+			yPositions.Clear();
+		}
+	}
+}
