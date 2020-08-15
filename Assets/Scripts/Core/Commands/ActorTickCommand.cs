d27c6c915dae6b913c193b293df95eed0af47189
Julio Labarca <jlabarca25@gmail.com>
2020-08-15 14:45:12 +0100

    ActorTickCommand

---
+++ Assets/Scripts/Core/Commands/ActorTickCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace com.jlabarca.cpattern.Core.Commands
+{
+    public class ActorTickCommand : ICommand
+    {
+        private Actor _actor;
+        private float _smooth;
+
+        public ActorTickCommand(Actor actor, float smooth)
+        {
+            _actor = actor;
+            _smooth = smooth;
+        }
+
+        public void Execute()
+        {
+            _actor.Tick(_smooth);
+        }
+    }
+}
